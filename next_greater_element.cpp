086285ed283c4ab670c5604b5ff41098b5f02133
Antor Mahmud <54511544+osman-mahmud-antor@users.noreply.github.com>
2022-02-07 05:30:47 -1200

    Create next_greater_element.cpp

---
+++ next_greater_element.cpp
@@ -0,0 +1,26 @@
+// Simple C++ program to print
+// next greater elements in a array
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;++i){
+        cin>>arr[i];
+    }
+    int next,i,j;
+    for(i=0;i<n;++i){
+        next=-1;
+        for(j=i+1;j<n;++j){
+            if(arr[i]<arr[j]){
+                next=arr[j];
+                break;
+            }
+        }
+        cout<<arr[i]<<"-->"<<next<<endl;
+    }
+    return 0;
+}
